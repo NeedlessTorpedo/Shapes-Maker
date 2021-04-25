ef7d9153fa9a7f9747e231f19e0c1d45cfecd50b
NeedlessTorpedo <thexylonmenh05@gmail.com>
2021-04-25 09:50:25 +0800

    Draws a Polygon based on user's inputs

--- DrawPolygon.py
+++ DrawPolygon.py
@@ -1,22 +1,23 @@
 import turtle
 import time
 
+#Draw Polygon Function
 def drawPolygon(wn, turtleName, sideLength, sides):
-    wn = turtle.Screen()
-    wn.bgcolor("lightgreen")
-    turtleName = turtle.Turtle()
-    turtleName.speed(0)
-
-    angleTurn = 360/sides
+    wn = turtle.Screen()                        #Creates a tkinter window
+    wn.bgcolor("lightgreen")                    #Background Color
+    turtleName = turtle.Turtle()                #Creates the turtle
+    turtleName.speed(0)                         #Speed of the turtle
+    angleTurn = 360/sides                       #Degree of angle
 
     for _ in range(sides):
         turtleName.left(angleTurn)
         turtleName.forward(sideLength)
     wn.exitonclick()
 
+#Draw Circle Function
 def drawCircle(turtleName, radius):
-    circumference = 2 * 3.14 * radius
-    sideLength = circumference / 360
+    circumference = 2 * 3.14 * radius           #Circumference of the circle
+    sideLength = circumference / 360            #Length of each side of the circle
     drawPolygon('wn', turtleName, sideLength, 360)
 
 
@@ -24,11 +25,13 @@ def drawCircle(turtleName, radius):
 
 typeOfShape = input("What type of shape do you want?? ((Polygon // Circle))")
 
+#Output if polygon
 if typeOfShape == 'Polygon':
     sideLength = int(input("Input length of sides here >>"))
     sides = int(input("Input the number of sides here >>"))
     drawPolygon('wn','john', sideLength, sides)
 
+#Output if circle
 elif typeOfShape == 'Circle':
     radius = int(input("Input the number of radius here >>"))
     drawCircle('john', radius)
